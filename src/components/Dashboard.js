d747deb6d51650b570aab81c6657adb593458c28
Igor Kakupshev <ivkakupshev@yandex.ru>
2021-02-04 19:57:46 +0500

    fix: return the old name of api.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,7 +10,7 @@ import AddPlacePopup from './AddPlacePopup';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 
-import { api } from '../utils/appApi';
+import { api } from '../utils/api';
 
 function Dashboard(props) {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
